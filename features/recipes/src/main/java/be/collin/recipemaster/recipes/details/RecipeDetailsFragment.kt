227e602583a3767421d4714834f03b331d6ba580
collin <collin.busschots@capgemini.com>
2021-07-25 15:22:46 +0200

    Update details title

--- features/recipes/src/main/java/be/collin/recipemaster/recipes/details/RecipeDetailsFragment.kt
+++ features/recipes/src/main/java/be/collin/recipemaster/recipes/details/RecipeDetailsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import be.collin.recipemaster.recipes.BitmapFactory
@@ -19,15 +20,25 @@ class RecipeDetailsFragment : Fragment(R.layout.fragment_recipe_details) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.recipeDetails.observe(viewLifecycleOwner, {
+            updateUIValues(view, it)
+            updateToolbar(it.title)
+        })
+    }
+
+    private fun updateUIValues(view: View, uiModel: RecipeDetailsUIModel) {
         val title = view.findViewById<TextView>(R.id.recipeTitle)
         val image = view.findViewById<ImageView>(R.id.recipeImage)
         val duration = view.findViewById<TextView>(R.id.recipeDuration)
 
-        viewModel.recipeDetails.observe(viewLifecycleOwner, {
-            title.text = it.title
-            val bitmap = BitmapFactory.fromBase64Bytes(it.imageContent)
-            image.setImageBitmap(bitmap)
-            duration.text = it.duration
-        })
+        title.text = uiModel.title
+        val bitmap = BitmapFactory.fromBase64Bytes(uiModel.imageContent)
+        image.setImageBitmap(bitmap)
+        duration.text = uiModel.duration
+    }
+
+    private fun updateToolbar(title: String) {
+        (requireActivity() as AppCompatActivity).supportActionBar?.title = title
     }
 }
