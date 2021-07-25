3e306f9cbf86a5f535fa6bf56a1094208eda5daa
collin <collin.busschots@capgemini.com>
2021-07-25 14:35:14 +0200

    Add tests for Recipes

---
+++ features/recipes/src/test/java/be/collin/recipemaster/recipes/RecipesTest.kt
@@ -0,0 +1,44 @@
+package be.collin.recipemaster.recipes
+
+import be.collin.recipemaster.recipes.overview.Recipes
+import be.collin.recipemaster.recipes.overview.recipeArb
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.property.arbitrary.next
+
+class RecipesTest : StringSpec({
+    "recipeWithName should return correct recipe" {
+        val first = recipeArb.next().copy("first")
+        val second = recipeArb.next().copy("second")
+        val third = recipeArb.next().copy("third")
+        val recipes = Recipes(
+            listOf(
+                first,
+                second,
+                third,
+            )
+        )
+
+        val recipe = recipes.recipeWithName("second")
+
+        recipe shouldBe second
+    }
+
+    "recipeWithName should throw exception if recipe does not exist" {
+        val first = recipeArb.next().copy("first")
+        val second = recipeArb.next().copy("second")
+        val third = recipeArb.next().copy("third")
+        val recipes = Recipes(
+            listOf(
+                first,
+                second,
+                third,
+            )
+        )
+
+        shouldThrow<NoSuchElementException> {
+            recipes.recipeWithName("fourth")
+        }
+    }
+})
