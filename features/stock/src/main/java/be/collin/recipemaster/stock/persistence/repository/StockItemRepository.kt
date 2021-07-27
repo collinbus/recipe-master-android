5172c918531d87a4fe4980bef96927ebceabb561
collin <collin.busschots@capgemini.com>
2021-07-27 15:37:28 +0200

    Call insert on dao when refrigerator items are added

--- features/stock/src/main/java/be/collin/recipemaster/stock/persistence/repository/StockItemRepository.kt
+++ features/stock/src/main/java/be/collin/recipemaster/stock/persistence/repository/StockItemRepository.kt
@@ -4,15 +4,33 @@ import be.collin.recipemaster.stock.Quantity
 import be.collin.recipemaster.stock.StockItem
 import be.collin.recipemaster.stock.StockItems
 import be.collin.recipemaster.stock.persistence.dao.RefrigeratorDao
+import be.collin.recipemaster.stock.persistence.entities.StockItemEntity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class StockItemRepository(private val refrigeratorDao: RefrigeratorDao) {
-    suspend fun getStockItems(): StockItems = StockItems(
+    suspend fun getRefrigeratorStockItems(): StockItems = StockItems(
         withContext(Dispatchers.IO) {
             refrigeratorDao.getRefrigeratorStockItems().map {
                 StockItem(it.id, it.name, Quantity(it.quantity))
             }
         }
     )
+
+    fun insertRefrigeratorStockItems(stockItems: StockItems) {
+        stockItems.forEach {
+            refrigeratorDao.addRefrigeratorStockItem(
+                StockItemEntity(
+                    it.id,
+                    it.name,
+                    it.quantity.value,
+                    REFRIGERATOR_STOCK_ITEM_TYPE
+                )
+            )
+        }
+    }
+
+    companion object {
+        private const val REFRIGERATOR_STOCK_ITEM_TYPE = 1
+    }
 }
--- features/stock/src/test/java/be/collin/recipemaster/stock/persistence/repository/StockItemRepositoryTest.kt
+++ features/stock/src/test/java/be/collin/recipemaster/stock/persistence/repository/StockItemRepositoryTest.kt
@@ -7,8 +7,10 @@ import be.collin.recipemaster.stock.persistence.dao.RefrigeratorDao
 import be.collin.recipemaster.stock.persistence.entities.StockItemEntity
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 
 internal class StockItemRepositoryTest : StringSpec({
     "should return StockItems when getRefrigeratorStockItems is called" {
@@ -27,7 +29,7 @@ internal class StockItemRepositoryTest : StringSpec({
         }
         val repository = StockItemRepository(refrigeratorDao)
 
-        val stockItems = repository.getStockItems()
+        val stockItems = repository.getRefrigeratorStockItems()
 
         stockItems shouldBe StockItems(
             listOf(
@@ -35,5 +37,17 @@ internal class StockItemRepositoryTest : StringSpec({
                 StockItem(id2, name2, Quantity(quantity2))
             )
         )
+    }
+
+    "should call insert on dao with correct stock item type when an item is added" {
+        val refrigeratorDao: RefrigeratorDao = mockk()
+        val repository = StockItemRepository(refrigeratorDao)
+        val id = "id"
+        val name = "name"
+        val quantity = Quantity(5)
+
+        repository.insertRefrigeratorStockItems(StockItems(listOf(StockItem(id, name, quantity))))
+
+        verify { refrigeratorDao.addRefrigeratorStockItem(StockItemEntity(id, name, quantity.value, 1)) }
     }
 })
