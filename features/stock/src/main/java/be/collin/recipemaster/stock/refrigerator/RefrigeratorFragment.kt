eabd33ac1ee05b4411cf4b24a268f103c8135bfd
collin <collin.busschots@capgemini.com>
2021-07-28 10:31:15 +0200

    Create refrigerator fragment that contains a stock item fragment

---
+++ features/stock/src/main/java/be/collin/recipemaster/stock/refrigerator/RefrigeratorFragment.kt
@@ -0,0 +1,6 @@
+package be.collin.recipemaster.stock.refrigerator
+
+import androidx.fragment.app.Fragment
+import be.collin.recipemaster.stock.R
+
+class RefrigeratorFragment : Fragment(R.layout.refrigerator_fragment)
